cb49169c8b3f0457ddc64aa6eedafd120c1f59cb
fabio-lucs <fabiolucs19@gmail.com>
2023-12-07 00:26:50 -0300

    up

--- breast_dataset/src/optimizers.py
+++ breast_dataset/src/optimizers.py
@@ -14,20 +14,26 @@ class OptimizerStrategy(abc.ABC):
     
 class NewtonsMethod(OptimizerStrategy):
 
+    def __init__(self, learning_rate: float):
+        super().__init__(learning_rate)
+
     def update_model(self, X, y, model: models.Model):
-        gradient = model.gradient(X , y)
-        
+        gradient = model.gradient(X , y)       
         hessian = model.hessian(X, y)
         hessian += self.learning_rate * np.eye(hessian.shape[0])
         hessian_inv = np.linalg.inv(hessian)
-        
+        gradient = (gradient[0].reshape(model.w.shape[0], model.w.shape[1]))
         model.w = model.w  - self.learning_rate * np.dot(hessian_inv, gradient)
         
 
 class SteepestDescentMethod(OptimizerStrategy):
 
+    def __init__(self, learning_rate: float):
+        super().__init__(learning_rate)
+
     def update_model(self, X, y, model: models.Model):
         gradient = model.gradient(X, y)
+        gradient = (gradient[0].reshape(model.w.shape[0],model.w.shape[1]))
         model.w = model.w - self.learning_rate * gradient
 
 
